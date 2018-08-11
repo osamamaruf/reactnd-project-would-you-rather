ad8b4097016e515c62ee71ff237fc773668bf829
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-12 03:40:56 +0500

    refactor: compile errors removed

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,4 +1,4 @@
-import {setAUthedUser, SET_AUTHED_USER} from '../actions/authedUser'
+import { SET_AUTHED_USER } from '../actions/authedUser'
 
 export default function users (state = null , action) {
     switch(action.type) {
