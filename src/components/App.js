df104e9d6a72b837ca22b83bc4e1fb274eed6426
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-14 13:32:17 +0500

    feat: Home component basic commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Home from './Home'
 
 class App extends Component {
 
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class Home extends Component{
+    render() {
+        console.log(this.props)
+        return(
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ questions }){
+    return {
+      questionIds: Object.keys(questions)
+        .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+    }
+  }
+
+export default connect(mapStateToProps)(Home)
