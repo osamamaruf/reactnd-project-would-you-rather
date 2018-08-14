71a2e8ed3a4004fccd2f55dfc94c37c395efc3a5
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-15 02:46:32 +0500

    feat: Toolbar and Answered/ Unanswered tab added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Question from './Question'
+import QuestionList from './QuestionList'
+import Toolbar from './Toolbar'
+import { Tabs, Tab } from 'react-bootstrap'
 
 class Home extends Component{
     render() {
-        const { questionIds } = this.props;
+        const { questionIds, questions, authedUser } = this.props;
         return(
-            <div className="container">                
-                {
-                    questionIds.map((questionId) => (                            
-                        <Question key={questionId} id={questionId} />                            
-                    ))
-
-                }                                         
+            <div>  
+                <Toolbar />                             
+                <div className="container">                    
+                    <Tabs defaultActiveKey={1}>
+                        <Tab eventKey={1} title="Unanswered Questions">
+                            <QuestionList questionIds={questionIds.filter(id => !questions[id].optionOne.votes.concat(questions[id].optionTwo.votes).includes(authedUser))}/>                                   
+                        </Tab>
+                        <Tab eventKey={2} title="Answered Questions">
+                            <QuestionList questionIds={questionIds.filter(id => questions[id].optionOne.votes.concat(questions[id].optionTwo.votes).includes(authedUser))}/>                                   
+                        </Tab>                        
+                    </Tabs>                  
+                </div>
             </div>
         )
     }
 }
 
-function mapStateToProps({ questions }){
+function mapStateToProps({ questions , authedUser}){    
+                            
     return {
-      questionIds: Object.keys(questions)
-        .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+      questionIds: Object.keys(questions).sort((a,b) => questions[b].timestamp - questions[a].timestamp),
+      questions,
+      authedUser                    
     }
 }
 
