5ff0420296687164f9a261c5c532855039f28065
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-14 16:23:00 +0500

    feat: Assets folder removed as not needed ; images hosted

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '../assets/sarah.jpg',
+    avatarURL: 'https://i.imgur.com/pGgpQQq.jpg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: '../assets/tyler.jpg',
+    avatarURL: 'https://i.imgur.com/zCrTGYc.jpg',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: '../assets/pp.jpg',
+    avatarURL: 'https://i.imgur.com/FgN1iWH.jpg',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
