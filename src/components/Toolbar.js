8533bc32c0fc59b25513a2c226f909bc4a9bf034
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-15 21:38:04 +0500

    refactor: links corrected

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -7,23 +7,23 @@ class Toolbar extends Component{
             <Navbar>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#brand">Would You Rather?</a>
+                        <a href="/">Would You Rather?</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>     
                 <Nav>
-                    <NavItem eventKey={1} href="#">
+                    <NavItem eventKey={1} href="/">
                     Home
                     </NavItem>
-                    <NavItem eventKey={2} href="#">
+                    <NavItem eventKey={2} href="/add">
                     New Question
                     </NavItem>    
-                    <NavItem eventKey={2} href="#">
+                    <NavItem eventKey={2} href="/leaderboard">
                     Leader Board
                     </NavItem>  
                 </Nav>  
                 <Nav pullRight>
-                    <NavItem eventKey={1} href="#">
+                    <NavItem eventKey={1} href="/logout">
                         Logout
                     </NavItem>                    
                 </Nav>         
