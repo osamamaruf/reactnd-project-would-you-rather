1f07d83bfa17a8de11d0b369b4b92c7454f1ec29
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-19 23:55:30 +0500

    feat: New question screen created

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Button, Label, Input, Form, FormGroup, Card, CardHeader, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 
 class NewPoll extends Component {
 
   render() {
     return (
-      <div>
-        New Poll
-      </div>
+
+      <Card>
+        <CardHeader>                  
+          <h4 className="text-center">Create New Question</h4>
+        </CardHeader>                                        
+        <CardBody body>            
+            <CardSubtitle>Complete the question below: </CardSubtitle>
+            <CardTitle>Would you rather...</CardTitle>
+            <Form>
+              <FormGroup>          
+                <Input type="text" id="optionOneText" placeholder="Enter option one text here." />
+              </FormGroup>
+              <div className="text-center">
+                <Label>or</Label>
+              </div>
+              <FormGroup>          
+                <Input type="text" id="optionTwoText" placeholder="Enter option two text here." />
+              </FormGroup>
+              <Button>Submit</Button>
+            </Form>
+        </CardBody>
+      </Card>      
     );
   }
 }
