7697e8121cab8001e55fbd205300690bf57c1130
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-19 13:41:49 +0500

    feat: Save answer reducer and action made

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,35 @@
+import { _saveQuestionAnswer } from '../utils/_DATA'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER'
 
 export function receiveQuestions (questions) {
     return {
         type : RECEIVE_QUESTIONS,
         questions
     }
+}
+
+
+function saveQuestionAnswer ({ authedUser, qid, answer }) {
+    return {
+        type : SAVE_QUESTION_ANSWER,
+        authedUser,
+        qid,
+        answer
+    }
+}
+
+export function handleSaveQuestionAnswer (info) {
+    return (dispatch) => {
+        dispatch(saveQuestionAnswer(info))
+
+        return _saveQuestionAnswer(info)
+            .catch((e) => {
+                console.warn('Error in handleSaveQuestionAnser: ', e)
+                dispatch(saveQuestionAnswer(info))
+                alert('There was an error while saving the Answer. Try again.')
+            })
+
+    }    
 }
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import {RECEIVE_QUESTIONS} from '../actions/questions'
+import { RECEIVE_QUESTIONS, SAVE_QUESTION_ANSWER } from '../actions/questions'
 
 export default function questions (state = {} , action) {
     switch(action.type) {
@@ -7,6 +7,20 @@ export default function questions (state = {} , action) {
                 ...state,
                 ...action.questions
             }
+        case SAVE_QUESTION_ANSWER:
+            return {
+                ...state,
+                [action.qid]: {
+                    ...state[action.qid],                
+                    [action.answer]: {
+                        ...state[action.qid][action.answer],
+                        votes: action.answer === 'optionOne'
+                        ? state[action.qid].optionOne.votes.concat([action.authedUser])
+                        : state[action.qid].optionTwo.votes.concat([action.authedUser])
+                    }
+
+                }
+            }            
         default :
             return state
     }
