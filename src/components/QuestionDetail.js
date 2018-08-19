f787e6398dfd7bb0e4178704029a67b28e3767ab
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-19 22:36:49 +0500

    feat: Unanswered and Answered views shown

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import UnAnsweredPoll from './UnAnsweredPoll'
+import AnsweredPoll from './AnsweredPoll'
 import { formatQuestion } from '../utils/helpers'
-import { Card, Button, CardHeader, CardBody, Media, Label, Input, Form, FormGroup } from 'reactstrap'
+import { Card, CardHeader, CardBody, Media } from 'reactstrap'
 import { formatDate } from '../utils/helpers'
-import { handleSaveQuestionAnswer } from '../actions/questions'
 
 class QuestionDetail extends Component {
 
-  state = {
-        answer:''
-  }
-
-  handleAnswer= (e) => {
-    e.preventDefault()
-
-    const { dispatch, authedUser, question} = this.props
-
-    dispatch(handleSaveQuestionAnswer({
-        authedUser,
-        qid: question.id,
-        answer: this.state.answer
-
-    }))
-  }
-
-  handleRadioToggle(value){
-    this.setState({answer: value});
-  }
-
   render() {            
     const { question } = this.props
-    const { name, timestamp, avatar, optionOneText, optionTwoText } = question
+    const { name, timestamp, avatar, hasAnswered } = question
     return(         
           <Card>
           <CardHeader>
@@ -42,25 +22,11 @@ class QuestionDetail extends Component {
                   <Media left top>
                     <Media object src={avatar} height={64} width={64} alt="avatar" className="rounded-circle"/>
                   </Media>
-                  <Media body>
-                    <Form>     
-                        <FormGroup tag="fieldset">
-                            <legend>Would You Rather?</legend>
-                            <FormGroup check>
-                                <Label check>
-                                <Input type="radio" name="vote" onClick={() => this.handleRadioToggle('optionOne')}  />{' '}
-                                {optionOneText}
-                                </Label>
-                            </FormGroup>
-                            <FormGroup check>
-                                <Label check>
-                                <Input type="radio" name="vote" onClick={() => this.handleRadioToggle('optionTwo')}/>{' '}
-                                {optionTwoText}
-                                </Label>
-                            </FormGroup>                            
-                        </FormGroup>                  
-                        <Button onClick={(e) => this.handleAnswer(e)}>Submit</Button>
-                    </Form>                                                                                                                                             
+                  <Media body> 
+                    {   hasAnswered ? 
+                        <AnsweredPoll question = {question} />:
+                        <UnAnsweredPoll question = {question} />    
+                    }                                           
                   </Media>
               </Media>                                                              
           </CardBody>                                        
