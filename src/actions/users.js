69b02b32a5deba07ba4eb36f64c8a69a58f32cbb
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-20 21:55:41 +0500

    feat: save user answer action and reducer added

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 export const SAVE_USER_QUESTION = 'SAVE_USER_QUESTION'
+export const SAVE_USER_ANSWER = 'SAVE_USER_ANSWER'
 
 export function receiveUsers (users) {
     return {
@@ -14,5 +15,15 @@ export function saveUserQuestion ({user, qid}) {
         type : SAVE_USER_QUESTION,        
         user,
         qid        
+    }    
+}
+
+
+export function saveUserAnswer ({authedUser, qid, answer}) {
+    return {
+        type : SAVE_USER_ANSWER,        
+        authedUser,
+        qid,
+        answer        
     }    
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS, SAVE_USER_QUESTION } from '../actions/users'
+import { RECEIVE_USERS, SAVE_USER_QUESTION, SAVE_USER_ANSWER } from '../actions/users'
 
 export default function users (state = {} , action) {
     switch(action.type) {
@@ -13,6 +13,17 @@ export default function users (state = {} , action) {
                 [action.user]: {
                     ...state[action.user],                
                     questions: state[action.user].questions.concat([action.qid])
+                }
+            }
+        case SAVE_USER_ANSWER:
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],                
+                    answers: {
+                        ...state[action.authedUser].answers,
+                        [action.qid]: action.answer
+                    }
                 }
             }
         default :
