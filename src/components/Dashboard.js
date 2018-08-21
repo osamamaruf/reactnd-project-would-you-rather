5d7928e1f6fd7e3ead276716ed6a25cdf36a8653
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-22 02:59:08 +0500

    refactor: stateless component made

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,19 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import Home from './Home'
 import NewPoll from './NewPoll'
 import LeaderBoard from './LeaderBoard'
 import QuestionDetail from './QuestionDetail'
 
-class Dashboard extends Component{
-    render() {        
-        return(                                                                   
-            <Fragment>                
-                <Route path='/' exact component={Home}/>
-                <Route path='/question/:id' component={QuestionDetail}/>
-                <Route path='/add' component={NewPoll}/>
-                <Route path='/leaderboard' component={LeaderBoard}/>                    
-            </Fragment>                    
-        )
-    }
+function Dashboard (){
+    return(                                                                   
+        <Fragment>                
+            <Route path='/' exact component={Home}/>
+            <Route path='/question/:id' component={QuestionDetail}/>
+            <Route path='/add' component={NewPoll}/>
+            <Route path='/leaderboard' component={LeaderBoard}/>                    
+        </Fragment>                    
+    )
 }
-
 
 export default Dashboard
