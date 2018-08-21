2b39d8a1ddd44bbd795a11d99a2bdaf1fbd9a0c5
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-21 23:43:24 +0500

    feat: added home url on 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class PageNotFound extends Component {
 
   render() {    
     return (
-        <div>
-            Page Not Found.
+        <div className="mt-4">
+            <center>
+              <div>404</div>
+              <Link to="/">Return to Home Page</Link>
+            </center>
         </div>
     );
   }
