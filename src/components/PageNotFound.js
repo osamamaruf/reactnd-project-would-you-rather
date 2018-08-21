e9fb51370a193cae794d7aba0722d121f8c80989
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-22 02:59:24 +0500

    refactor: stateless component made

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
-class PageNotFound extends Component {
-
-  render() {    
-    return (
-        <div className="mt-4">
-            <center>
-              <div>404</div>
-              <Link to="/">Return to Home Page</Link>
-            </center>
-        </div>
-    );
-  }
+function PageNotFound (){
+  return (
+    <div className="mt-4">
+        <center>
+          <div>404</div>
+          <Link to="/">Return to Home Page</Link>
+        </center>
+    </div>
+);
 }
 
 export default PageNotFound;
