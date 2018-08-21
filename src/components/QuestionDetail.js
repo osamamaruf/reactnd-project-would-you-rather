55804d9c1986f9e0c253813439b25f1ad8be6f55
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-22 02:59:30 +0500

    refactor: stateless component made

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import UnAnsweredPoll from './UnAnsweredPoll'
 import AnsweredPoll from './AnsweredPoll'
@@ -6,38 +6,35 @@ import { formatQuestion } from '../utils/helpers'
 import { Card, CardHeader, CardBody, Media } from 'reactstrap'
 import { formatDate } from '../utils/helpers'
 
-class QuestionDetail extends Component {
-
-  render() {            
-    const { question } = this.props
-    const { name, timestamp, avatar, hasAnswered } = question
-    return(         
-          <Card className="mt-4">
-          <CardHeader>
+function QuestionDetail (props) {
+  const { question } = props
+  const { name, timestamp, avatar, hasAnswered } = question
+  return(         
+        <Card className="mt-4">
+        <CardHeader>
+                {   hasAnswered ? 
+                      <h5>Asked by {name}</h5>:
+                      <div>
+                        <h4>{name} asks : </h4>
+                        <h5>{formatDate(timestamp)}</h5>
+                      </div>
+                }    
+        </CardHeader>                                        
+        <CardBody>
+            <Media>
+                <Media left top>
+                  <Media object src={avatar} height={64} width={64} alt="avatar" className="rounded-circle"/>
+                </Media>
+                <Media body> 
                   {   hasAnswered ? 
-                        <h5>Asked by {name}</h5>:
-                        <div>
-                          <h4>{name} asks : </h4>
-                          <h5>{formatDate(timestamp)}</h5>
-                        </div>
-                  }    
-          </CardHeader>                                        
-          <CardBody>
-              <Media>
-                  <Media left top>
-                    <Media object src={avatar} height={64} width={64} alt="avatar" className="rounded-circle"/>
-                  </Media>
-                  <Media body> 
-                    {   hasAnswered ? 
-                        <AnsweredPoll question = {question} />:
-                        <UnAnsweredPoll question = {question} />    
-                    }                                           
-                  </Media>
-              </Media>                                                              
-          </CardBody>                                        
-      </Card>            
-    )
-}
+                      <AnsweredPoll question = {question} />:
+                      <UnAnsweredPoll question = {question} />    
+                  }                                           
+                </Media>
+            </Media>                                                              
+        </CardBody>                                        
+    </Card>            
+  );
 }
 
 function mapStateToProps({authedUser, users, questions}, ownProps) {  
