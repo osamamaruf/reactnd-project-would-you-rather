cdbb57a1eb657cd14d7028033d8e026a7f18ee36
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-22 03:17:51 +0500

    fix: percentage formula corrected

--- src/components/AnsweredPoll.js
+++ src/components/AnsweredPoll.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 import { Progress, Card, CardText, CardTitle, Badge } from 'reactstrap'
 
 class AnsweredPoll extends Component {
 
   answerPercentage (optionText) {
-    const { question, users } = this.props
+    const { question } = this.props
     let numberOfVotesForOption = question.optionOneText === optionText ? 
             question.optionOne.votes.length : question.optionTwo.votes.length
-    let numberOfVotes = Object.keys(users).length;    
+    let numberOfVotes = question.totalVotesCount;    
     return (numberOfVotesForOption / numberOfVotes * 100).toFixed(2);
   }
 
   render () {
-    const { question, users } = this.props
-    const { optionOneText, optionTwoText, id, optionOneAnswered } = question
+    const { question } = this.props
+    const { optionOneText, optionTwoText, id, optionOneAnswered, totalVotesCount } = question
     return (
       <div>        
         <div>Results : </div>
@@ -23,10 +22,10 @@ class AnsweredPoll extends Component {
               { optionOneText } 
               { optionOneAnswered && <Badge className="float-right" color="success" pill>Your Answer!</Badge> }
             </CardTitle>
-            <Progress striped color="info" value={this.answerPercentage(id, optionOneText)} max="100">
-                {this.answerPercentage(id, optionOneText)}
+            <Progress striped color="info" value={this.answerPercentage(optionOneText)} max="100">
+                {this.answerPercentage(optionOneText)}
             </Progress>
-            <CardText className="text-center">{question.optionOne.votes.length} out of {Object.keys(users).length}</CardText>        
+            <CardText className="text-center">{question.optionOne.votes.length} out of {totalVotesCount}</CardText>        
           </Card>
         
           <Card body className="text-left mt-4">
@@ -34,21 +33,14 @@ class AnsweredPoll extends Component {
               { optionTwoText }
               { !optionOneAnswered && <Badge className="float-right" color="success" pill>Your Answer!</Badge> }
             </CardTitle>
-            <Progress striped color="info" value={this.answerPercentage(id, optionTwoText)} max="100">
-                {this.answerPercentage(id, optionTwoText)}
+            <Progress striped color="info" value={this.answerPercentage(optionTwoText)} max="100">
+                {this.answerPercentage(optionTwoText)}
             </Progress>
-            <CardText className="text-center">{question.optionTwo.votes.length} out of {Object.keys(users).length}</CardText>        
+            <CardText className="text-center">{question.optionTwo.votes.length} out of {totalVotesCount}</CardText>        
           </Card>                        
       </div>
     );
   }
 }
 
-function mapStateToProps({ users }, ownProps) {      
-    return {      
-      question : ownProps.question,
-      users
-    }
-  }
-
-export default connect(mapStateToProps)(AnsweredPoll);
+export default AnsweredPoll;
