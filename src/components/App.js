167b22b7e0ad06241cf17ba192d5f50b7bcde1e5
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-21 17:58:14 +0500

    refactor: toolbar, browser router moved outside of dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import Toolbar from './Toolbar'
 import LandingPage from './LandingPage'
-
 
 class App extends Component {
 
@@ -14,12 +15,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <LoadingBar/>
-        {this.props.loading === true
-          ? <LandingPage />
-          : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar/>                    
+              {this.props.loading === true
+                ? <Fragment>
+                    <div className="container"> 
+                      <Route path='/' exact component={LandingPage}/>
+                    </div>
+                  </Fragment>
+                : <Fragment>
+                    <Toolbar />  
+                    <Dashboard />
+                  </Fragment>                        
+              }          
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Toolbar from './Toolbar'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import Home from './Home'
 import NewPoll from './NewPoll'
 import LeaderBoard from './LeaderBoard'
@@ -8,18 +7,13 @@ import QuestionDetail from './QuestionDetail'
 
 class Dashboard extends Component{
     render() {        
-        return(
-            <Router>
-                <Fragment>  
-                    <Toolbar />                             
-                    <div className="container">
-                        <Route path='/' exact component={Home}/>
-                        <Route path='/question/:id' component={QuestionDetail}/>
-                        <Route path='/add' component={NewPoll}/>
-                        <Route path='/leaderboard' component={LeaderBoard}/>                        
-                    </div>
-                </Fragment>
-            </Router>
+        return(                                                                   
+            <div className="container">                
+                <Route path='/' exact component={Home}/>
+                <Route path='/question/:id' component={QuestionDetail}/>
+                <Route path='/add' component={NewPoll}/>
+                <Route path='/leaderboard' component={LeaderBoard}/>                    
+            </div>                    
         )
     }
 }
